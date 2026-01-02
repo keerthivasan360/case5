186ab7fe77eee79c43e9704665f77c2dcc2ce384
keerthivasan360 <keerthivasank83@gmail.com>
2026-01-02 17:07:27 +0530

    main: update student message

--- studentmodule.py
+++ studentmodule.py
@@ -2,4 +2,4 @@ def register_student(name, email):
     if "@" not in email:
         print("Invalid email")
     else:
-        print("Student Registered Successfully")
+        print("student Registered Successfully From Main branch")
